75299fd56579216a55126168256bb039e75996bf
awsdevops345 <naveena838693@gmail.com>
2024-01-26 13:36:28 +0530

    last change

--- 10-roboshop.sh
+++ 10-roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-AMI=ami-03265a0778a880afb
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+AMI=ami-0f3c7d07486cad139
+SG_ID=sg-0bcea092488fd766a #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z08332551BKOXNZEDPRQB # replace your zone ID
 DOMAIN_NAME="learnaws.online"
@@ -13,7 +13,7 @@ do
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
-    fi
+    fi 
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0bcea092488fd766a --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
